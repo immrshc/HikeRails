ece5b2c1218b126c43369fde94d3ba2b0f4a7e73
Shoichi Imamura <syoichiimamura@gmail.com>
2015-12-14 21:24:24 +0900

    ユーザモデルを微調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ActiveRecord::Base
 	has_secure_password
 	
 	validates :username, presence: true
+
+	def before_create
+    	token = Digest::SHA1.hexdigest("#{id}#{rand.to_s}")[0..15]
+    	write_attribute 'token', token
+  	end
 	
 	def self.auth(username, password)
 		user = find_by(username: username)
@@ -12,5 +17,6 @@ class User < ActiveRecord::Base
 			return
 		end
 	end
+
 
 end
